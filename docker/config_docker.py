39f93497b57bdc65523c841f9c3464755bdf2bbc
Ben Du <longendu@yahoo.com>
2026-04-27 17:56:56 -0700

    move build_images.py and config_docker.py from legendu-net/docker to this repo

---
+++ docker/config_docker.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env -S uv run
+#
+# /// script
+# requires-python = "==3.13"
+# dependencies = []
+# ///
+
+import json
+from pathlib import Path
+import subprocess as sp
+
+
+def config_data_root(data_root: str) -> dict[str, str]:
+    Path(data_root).mkdir(parents=True, exist_ok=True)
+    settings = {}
+    path = Path("/etc/docker/daemon.json")
+    if path.is_file():
+        with path.open("r", encoding="utf-8") as fin:
+            settings = json.load(fin)
+    settings["data-root"] = data_root
+    with path.open("w", encoding="utf-8") as fout:
+        json.dump(settings, fout, indent=4)
+    return settings
+
+
+def store_docker_on_mnt():
+    sp.run("systemctl stop docker", shell=True, check=True)
+    settings = config_data_root("/mnt/docker")
+    print(settings)
+    sp.run("systemctl start docker", shell=True, check=True)
+    sp.run("docker info", shell=True, check=True)
+
+
+if __name__ == "__main__":
+    store_docker_on_mnt()
